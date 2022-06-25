106c96c820edfa4f318ab82e72d54c1f1bfa8c8e
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-06-25 17:12:49 +0100

    logic

---
+++ src/notePad/Arithmetic.java
@@ -0,0 +1,28 @@
+package notePad;
+
+import java.util.Scanner;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int number2 = input.nextInt();
+
+        int Sum = number1 + number2;
+        System.out.printf("Sum is %d%n", +(Sum));
+
+        int Product = number1 * number2;
+        System.out.printf("Product is %d%n", +(number1 * number2));
+
+        int Difference = number1 - number2;
+        System.out.printf("Difference is %d%n", +(number1 - number2));
+
+        int Quotient = number1 / number2;
+        System.out.printf("Quotient is %d%n", +(number1 / number2));
+    }
+}
