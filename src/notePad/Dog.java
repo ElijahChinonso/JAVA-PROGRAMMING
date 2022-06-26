a3e696593924df2f5147b393f79e995506946ccb
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-06-26 13:28:21 +0100

    My Dog

---
+++ src/notePad/Dog.java
@@ -0,0 +1,18 @@
+package notePad;
+
+public class Dog{
+    private String head;
+    private String eyes;
+    private String nose;
+    private int age;
+    public Dog(){}
+    public Dog(String head, String eyes, String nose, int age){
+        this.head = head;
+        this.eyes = eyes;
+        this.nose = nose;
+        this.age = age;
+    }
+    public static void main(String[] args){
+        System.out.println("My Dog");
+    }
+}
