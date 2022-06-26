d34091b08f5842797b1b680850a2112ad083feb9
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-06-26 13:12:27 +0100

    My cat

---
+++ src/notePad/Cat.java
@@ -0,0 +1,16 @@
+package notePad;
+
+public class Cat {
+    private String name;
+
+    public Cat(){}
+
+    public String setCatName(String jerry){
+        return jerry;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("The name of my Cat is Jerry");
+    }
+}
