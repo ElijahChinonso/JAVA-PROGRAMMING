f5589b7060eadb8ad419bcb7e7eb3e6b9ed8e49b
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-06-26 21:59:55 +0100

    finally did it

---
+++ src/bike/Bike.java
@@ -0,0 +1,263 @@
+package bike;
+
+public class Bike {
+
+    public String isOn(String on) throws Exception {
+        if (on == "on") {
+            System.out.println("Bike is on");
+        } else {
+            throw new Exception("Enter the right on");
+        }
+        return on;
+    }
+
+
+    public String isOff(String off) throws Exception {
+        if (off == "off") {
+            System.out.println("Bike is off");
+        }
+        else {
+            throw new Exception("Off properly");
+        }
+        return off;
+    }
+
+    public int accelerateGearOne(int gear , int speedLimit ) throws Exception {
+        if(gear == 1){
+            System.out.println("Hello you are on gear one");
+        }
+        else{
+            throw new Exception("Not on gear  1");
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit++;
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit+=2;
+            System.out.println("you are on gear two");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit+=3;
+            System.out.println("you are on gear three");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit+=4;
+            System.out.println("you are on gear four");
+        }
+        return speedLimit;
+    }
+
+
+    public int accelerateGearTwo(int gear , int speedLimit ) throws Exception {
+        if(gear == 2){
+            System.out.println("Hello you are on gear Two");
+        }
+        else{
+            throw new Exception("Not on gear  2");
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit+=2;
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit++;
+            System.out.println("you are on gear one");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            System.out.println("you are on gear three");
+            speedLimit+=3;
+        }
+
+        if (speedLimit >= 41){
+            speedLimit+=4;
+            System.out.println("you are on gear four");
+        }
+        return speedLimit;
+    }
+
+    public int accelerateGearThree(int gear , int speedLimit ) throws Exception {
+        if(gear == 3){
+            System.out.println("Hello you are on gear Three");
+        }
+        else{
+            throw new Exception("Not on gear  3");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit+=3;
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit++;
+            System.out.println("You are already on gear one");
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit+=2;
+            System.out.println("You are already on gear two");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit+=4;
+            System.out.println("you are already on gear four");
+        }
+        return speedLimit;
+    }
+
+    public int accelerateGearFour(int gear , int speedLimit ) throws Exception {
+        if(gear == 4){
+            System.out.println("Hello you are on gear Four");
+        }
+        else{
+            throw new Exception("Not on gear  4");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit+=4;
+        }
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit++;
+            System.out.println("You are already in gear one");
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit+=2;
+            System.out.println("You are already in gear two");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit+=3;
+            System.out.println("You are already in gear three");
+        }
+        return speedLimit;
+    }
+
+
+
+    public int decelerateGearOne(int gear , int speedLimit ) throws Exception {
+        if(gear == 1){
+            System.out.println("Hello you are on gear one");
+        }
+        else{
+            throw new Exception("Not on gear  1");
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit--;
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            System.out.println("you are now on gear two");
+            speedLimit-=2;
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit-=3;
+            System.out.println("you are now on gear three");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit-=4;
+            System.out.println("you are now on gear four");
+        }
+        return speedLimit;
+    }
+
+
+    public int decelerateGearTwo(int gear , int speedLimit ) throws Exception {
+        if(gear == 2){
+            System.out.println("Hello you are on gear two");
+        }
+        else{
+            throw new Exception("Not on gear  2");
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit--;
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit--;
+            System.out.println("you are now on gear one");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            System.out.println("you are now on gear three");
+            speedLimit-=3;
+
+        }
+
+        if (speedLimit >= 41){
+            speedLimit-=4;
+            System.out.println("you are now on gear four");
+        }
+        return speedLimit;
+    }
+
+
+    public int decelerateGearThree(int gear , int speedLimit ) throws Exception {
+        if(gear == 3){
+            System.out.println("Hello you are on gear three");
+        }
+        else{
+            throw new Exception("Not on gear  3");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit-=3;
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            speedLimit--;
+            System.out.println("you are now on gear one");
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit-=2;
+            System.out.println("you are now on gear two");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit-=4;
+            System.out.println("you are now on gear four");
+        }
+        return speedLimit;
+    }
+
+
+    public int decelerateGearFour(int gear , int speedLimit ) throws Exception {
+        if(gear == 4){
+            System.out.println("Hello you are on gear four");
+        }
+        else{
+            throw new Exception("Not on gear  4");
+        }
+
+        if (speedLimit >= 41){
+            speedLimit-=4;
+        }
+
+        if (speedLimit >= 0 && speedLimit <= 20){
+            System.out.println("you are now on gear one");
+            speedLimit--;
+        }
+
+        if (speedLimit >= 21 && speedLimit <= 30){
+            speedLimit-=2;
+            System.out.println("you are now on gear two");
+        }
+
+        if (speedLimit >= 31 && speedLimit <= 40){
+            speedLimit-=3;
+            System.out.println("you are now on gear three");
+        }
+        return speedLimit;
+    }
+}
+
---
+++ test/bike/BikeTest.java
@@ -0,0 +1,138 @@
+package bike;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BikeTest {
+
+    public Bike rider;
+
+    @BeforeEach
+    public void initSetup() { rider = new Bike();}
+
+    @Test
+    public void test_isOn() throws Exception{
+        String result = rider.isOn("on");
+        Assertions.assertEquals(result,"on");
+    }
+
+    @Test
+    public void test_isOnIsInvalid() throws Exception{
+        String result = rider.isOn("off");
+        Assertions.assertEquals(result,"off");
+    }
+
+
+    @Test
+    public void test_isOff() throws Exception{
+        String result = rider.isOff("off");
+        Assertions.assertEquals(result,"off");
+    }
+
+    @Test
+    public void test_isOffIsInvalid() throws Exception{
+        String result = rider.isOff("on");
+        Assertions.assertEquals(result,"on");
+    }
+
+
+    @Test
+    public void test_gearOne() throws Exception{
+        int result = rider.accelerateGearOne(1,18);
+        Assertions.assertEquals(result,19);
+    }
+
+    @Test
+    public void test_gearOneInvalid() throws Exception{
+        int result = rider.accelerateGearOne(1,32);
+        Assertions.assertEquals(result, 35);
+    }
+
+
+    @Test
+    public void test_gearTwo() throws Exception{
+        int result = rider.accelerateGearTwo(2,22);
+        Assertions.assertEquals(result,24);
+    }
+
+    @Test
+    public void test_gearTwoInvalid() throws Exception{
+        int result = rider.accelerateGearTwo(2,31);
+        Assertions.assertEquals(result, 34);
+    }
+
+    @Test
+    public void test_gearThree() throws Exception{
+        int result = rider.accelerateGearThree(3,32);
+        Assertions.assertEquals(result,35);
+    }
+
+    @Test
+    public void test_gearThreeInvalid() throws Exception{
+        int result = rider.accelerateGearThree(3,20);
+        Assertions.assertEquals(result, 21);
+    }
+
+
+    @Test
+    public void test_gearFour() throws Exception{
+        int result = rider.accelerateGearFour(4,42);
+        Assertions.assertEquals(result,46);
+    }
+
+    @Test
+    public void test_gearFourInvalid() throws Exception{
+        int result = rider.accelerateGearFour(4,35);
+        Assertions.assertEquals(result, 38);
+    }
+
+    @Test
+    public void test_decelerateGearOne() throws Exception{
+        int result = rider.decelerateGearOne(1,24);
+        Assertions.assertEquals(result, 22);
+    }
+
+    @Test
+    public void test_decelerateGearOneInvalid() throws Exception{
+        int result = rider.decelerateGearOne(1,35);
+        Assertions.assertEquals(result, 32);
+    }
+
+    @Test
+    public void test_decelerateGearTwo() throws Exception{
+        int result = rider.decelerateGearTwo(2,34);
+        Assertions.assertEquals(result, 31);
+    }
+
+    @Test
+    public void test_decelerateGearTwoInvalid() throws Exception{
+        int result = rider.decelerateGearTwo(2,35);
+        Assertions.assertEquals(result, 32);
+    }
+
+    @Test
+    public void test_decelerateGearThree() throws Exception{
+        int result = rider.decelerateGearThree(3,50);
+        Assertions.assertEquals(result, 46);
+    }
+
+    @Test
+    public void test_decelerateGearThreeInvalid() throws Exception{
+        int result = rider.decelerateGearThree(3,3);
+        Assertions.assertEquals(result, 2);
+    }
+
+
+    @Test
+    public void test_decelerateGearFour() throws Exception{
+        int result = rider.decelerateGearFour(4,50);
+        Assertions.assertEquals(result, 46);
+    }
+
+    @Test
+    public void test_decelerateGearFourInvalid() throws Exception{
+        int result = rider.decelerateGearFour(4,26);
+        Assertions.assertEquals(result, 24);
+    }
+}
