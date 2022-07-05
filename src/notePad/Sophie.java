e87b9caf0d150249e411a883cce82be180cf5ebf
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-07-05 16:21:54 +0100

    love

---
+++ src/notePad/Sophie.java
@@ -0,0 +1,15 @@
+package notePad;
+
+public class Sophie {
+    private String love;
+
+    public Sophie(){}
+
+    public String setSophieLove(String Jack){
+        return Jack;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Sophie loves Jack");
+    }
+}
