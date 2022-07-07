ca7e4f1f83f578be12ec4ed9602390a297cc44b4
ElijahChinonso <okougbeninelijah003@gmail.com>
2022-07-07 16:13:01 +0100

    Sum of array

---
+++ src/array/SumOfNumbers.java
@@ -0,0 +1,14 @@
+package array;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+        int[] array = {5,6,7,9,10};
+        int total = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        System.out.printf("Total of array elements:  %d%n", total);
+    }
+}
